ad8ad2b16f8b39e52d050fe9d4da7200e471bf1b
Jared Mates <jaredmates45@gmail.com>
2022-05-13 16:55:44 -0700

    added basic math operators

---
+++ index.js
@@ -0,0 +1,21 @@
+// Add
+function add(num1, num2) {
+  return num1 + num2;
+}
+// Subtract
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+// Multiply
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+// divide
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+console.log(add(5, 3));
+console.log(subtract(5, 3));
+console.log(multiply(5, 3));
+console.log(divide(5, 3));
