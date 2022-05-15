fc1ac002df3833ddd87cc235e687933deef8165e
Jared Mates <jaredmates45@gmail.com>
2022-05-15 15:48:45 -0700

    added functionality to the buttons

--- index.js
+++ index.js
@@ -15,23 +15,67 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-console.log(add(5, 3));
-console.log(subtract(5, 3));
-console.log(multiply(5, 3));
-console.log(divide(5, 3));
-
 function operate(operator, num1, num2) {
   if (operator === "+") {
-    return add(num1, num2);
+    return (displayValue.value = add(num1, num2));
   } else if (operator == "-") {
-    return subtract(num1, num2);
+    return (displayValue.value = subtract(num1, num2));
   } else if (operator == "*") {
-    return multiply(num1, num2);
+    return (displayValue.value = multiply(num1, num2));
   } else if (operator == "/") {
-    return divide(num1, num2);
+    return (displayValue.value = divide(num1, num2));
   } else {
     return console.log("Invalid operation");
   }
 }
 
-console.log(operate("/", 10, 2));
+const displayValue = document.querySelector(".calculator-screen");
+const buttons = document.querySelectorAll("button");
+const equalSign = document.querySelector(".equal-sign");
+const allClear = document.querySelector(".all-clear");
+
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    if (displayValue.value === "0") {
+      displayValue.value = button.value;
+    } else {
+      displayValue.value += button.value;
+    }
+  });
+});
+
+equalSign.addEventListener("click", () => {
+  if (displayValue.value.includes("+")) {
+    let str = displayValue.value;
+    let num1 = str.substring(0, str.indexOf("+"));
+    let num2 = str.substring(str.indexOf("+") + 1);
+    operate("+", parseInt(num1), parseInt(num2));
+  }
+
+  if (displayValue.value.includes("-")) {
+    let str = displayValue.value;
+    let num1 = str.substring(0, str.indexOf("-"));
+    let num2 = str.substring(str.indexOf("-") + 1);
+    operate("-", parseInt(num1), parseInt(num2));
+  }
+
+  if (displayValue.value.includes("*")) {
+    let str = displayValue.value;
+    let num1 = str.substring(0, str.indexOf("*"));
+    let num2 = str.substring(str.indexOf("*") + 1);
+    operate("*", parseInt(num1), parseInt(num2));
+  }
+
+  if (displayValue.value.includes("/")) {
+    let str = displayValue.value;
+    let num1 = str.substring(0, str.indexOf("/"));
+    let num2 = str.substring(str.indexOf("/") + 1);
+    operate("/", parseInt(num1), parseInt(num2));
+  }
+});
+
+// Reset screen to zero
+allClear.addEventListener("click", () => {
+  displayValue.value = 0;
+});
